35177ab1e7c499692386c1875d9a3f48d3d64fd1
Alessandro <balboalessandro@gmail.com>
2024-07-14 17:53:30 +0200

    pulsanti

--- functions.php
+++ functions.php
@@ -4,3 +4,23 @@ add_action( 'wp_enqueue_scripts', function () {
     wp_enqueue_style( 'style-name', get_stylesheet_uri() );
     //wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
 } );
+
+add_action( 'admin_enqueue_scripts', function (){
+    wp_enqueue_style( 'style-name', get_stylesheet_uri() );
+});
+
+add_filter('wp_theme_json_data_default', function ($json){
+    return $json;
+});
+
+add_filter('wp_theme_json_data_blocks', function ($json){
+    return $json;
+});
+
+add_filter('wp_theme_json_data_theme', function ($json){
+    return $json;
+});
+
+add_filter('wp_theme_json_data_user', function ($json){
+    return $json;
+});
