5b36754477275f49701f2c61abd442cf0a85ad27
Alessandro <balboalessandro@gmail.com>
2024-07-18 16:18:21 +0200

    added gulp for scss

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const { dest, src } = require("gulp");
+const sass = require("gulp-dart-sass");
+
+function scss() {
+    return src("style.scss")
+        .pipe(sass())
+        .pipe(dest("./"));
+}
+
+exports.scss = scss;
